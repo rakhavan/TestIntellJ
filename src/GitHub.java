4243eb0d98f286bbca8566454b37899813a3ce6e
johnxu2000 <john.xu@ashbury.ca>
2018-04-03 13:27:19 -0400

    Added addition() method

--- src/GitHub.java
+++ src/GitHub.java
@@ -5,4 +5,11 @@ public class GitHub {
     public static void foo() {
         System.out.println("We did it!");
     }
+
+    public static void addition(){
+        int num1 = 1;
+        int num2 = 2;
+        int num3 = num1 + num2;
+        System.out.println(num3);
+    }
 }
