9ea38afc75fe32265e3db1e1e58393a975e16a1a
johnxu2000 <john.xu@ashbury.ca>
2018-04-03 13:19:38 -0400

    updated sout

--- src/GitHub.java
+++ src/GitHub.java
@@ -2,5 +2,7 @@ public class GitHub {
     public static void main(String[] args){
         System.out.println("Updatedt");
     }
-    public static void foo() { }
+    public static void foo() {
+        System.out.println("We did it!");
+    }
 }
